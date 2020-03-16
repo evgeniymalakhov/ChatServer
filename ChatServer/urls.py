34c9fa84d34607288502ccbce3a9c754c22e3c3c
Evgeniy M <eugeniymalakhov@gmail.com>
2020-03-17 00:35:30 +0200

    little fix STATIC_ROOT setting

--- ChatServer/urls.py
+++ ChatServer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', include('chat.urls')),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
